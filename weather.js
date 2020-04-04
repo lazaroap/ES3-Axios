ceeef629ac04ad7c0d34e48d3b7d44c95e54c720
Lázaro Pereira <lazaroairespereira@gmail.com>
2020-04-04 19:35:46 -0300

    Trabalho Axios aula 4

---
+++ weather.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+class Api {
+    static async getWeather(city) {
+        const weatherNow = new Weather((await axios.get(`https://api.hgbrasil.com/weather?key=285c7a5a&city_name=${city}`)).data.results);
+    return weatherNow
+
+    }
+}
+
+class Weather{
+    constructor ({city_name, temp, date, condition_slug, currently, sunrise, sunset}){
+      this.city_name = city_name,
+      this.temperature = temp,
+      this.date = date,
+      this.condition_slug = condition_slug,
+      this.currently = currently,
+      this.sunrise = sunrise,
+      this.sunset = sunset
+    }
+  }
+
+Api.getWeather("Pelotas,RS").then(weather => {console.log(weather)})
+
+
+
+
+
+
+
+// 285c7a5a
